8aad8eb4963705964c6fae81305b435e36c65019
Luis Tellez <luis@luistellez.com>
2022-12-03 09:53:48 -0800

    feat: add Register form

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Register() {
-  return <h1>Register</h1>;
+  return (
+    <div className="auth">
+      <form className="auth__form">
+        <h2 className="modal__title auth__title">Register</h2>
+        <input
+          className="modal__input auth__input"
+          type="email"
+          placeholder="Email"
+        />
+        <input
+          className="modal__input auth__input"
+          type="password"
+          placeholder="Password"
+        />
+        <button className="modal__form-submit auth__submit">Sign up</button>
+        <Link to="/signin" className="auth__link">
+          Already a member? Log in here!
+        </Link>
+      </form>
+    </div>
+  );
 }
 
 export default Register;
