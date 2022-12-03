97d47873ab42bfa7af2c27566c8674bf7a209bca
Luis Tellez <luis@luistellez.com>
2022-12-03 12:21:05 -0800

    style: adjust tooltip spacing

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -1,10 +1,14 @@
 import React from "react";
+import SuccessMark from "../images/success-mark.svg";
 
 function InfoTooltip() {
   return (
     <div className="modal modal_active">
       <div className="modal__container">
-        <h2 className="modal__title">Success! You have now been registered.</h2>
+        <img className="modal__tooltip-image" src={SuccessMark} alt="Success" />
+        <h3 className="modal__title modal__tooltip-text">
+          Success! You have now been registered.
+        </h3>
         <button className="modal__close-button modal__close_tooltip" />
       </div>
     </div>
