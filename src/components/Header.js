49709ccf1769d38112b8b7d6f84a08dc9b3d14ec
Luis Tellez <luis@luistellez.com>
2022-12-03 10:58:37 -0800

    style: add styling to nav element on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,14 @@ function Header() {
   return (
     <>
       <header className="main__container header">
-        <img className="header__logo" src={logo} alt="Logo Around" />
-        <Link to="/signin" className="header__auth">
-          Log in
+        <Link to="/">
+          <img className="header__logo" src={logo} alt="Logo Around" />
         </Link>
+        <nav className="header__nav">
+          <Link to="/signin" className="header__auth">
+            Log in
+          </Link>
+        </nav>
       </header>
       <div className="header__line"></div>
     </>
