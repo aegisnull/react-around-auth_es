2eacd74ff9377563cecd288b59a0bb013c1e1948
Luis Tellez <luis@luistellez.com>
2022-12-02 17:21:47 -0800

    feat: add html markup for Login.js form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Login() {
-  return <h1>Login</h1>;
+  return (
+    <div className="login">
+      <form className="login__form">
+        <h2 className="modal__title login__title">Login</h2>
+        <input
+          className="modal__input login__input"
+          type="email"
+          placeholder="Email"
+        />
+        <input
+          className="modal__input login__input"
+          type="password"
+          placeholder="Password"
+        />
+        <button className="modal__form-submit login__submit">Log in</button>
+        <Link to="/signup" className="login__link">
+          Not a member yet? Sign up here!
+        </Link>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
