fccbf4965f24bc1aea70266abf3e59c1a0066d42
Luis Tellez <luis@luistellez.com>
2022-12-03 10:18:09 -0800

    refactor: decouple header to have flexbox and header__line to be independent from header element

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import logo from "../images/logo.svg";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <header className="main__container header">
-      <img className="header__logo" src={logo} alt="Logo Around" />
+    <>
+      <header className="main__container header">
+        <img className="header__logo" src={logo} alt="Logo Around" />
+        <Link to="/signin" className="header__auth">
+          Log in
+        </Link>
+      </header>
       <div className="header__line"></div>
-    </header>
+    </>
   );
 }
 
