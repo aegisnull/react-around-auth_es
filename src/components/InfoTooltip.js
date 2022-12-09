6a36c6f6d99b3f158dd18310032d8c3d9161e369
Luis Tellez <luis@luistellez.com>
2022-12-08 20:48:57 -0800

    feat: make InfoTooltip variable according to situation

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -1,16 +1,21 @@
 import React from "react";
 import SuccessMark from "../images/success-mark.svg";
+import FailMark from "../images/failed-mark.svg";
 
 function InfoTooltip(props) {
+  const image = props.isSuccess ? SuccessMark : FailMark;
+  const text = props.isSuccess
+    ? "Success! You have now been registered."
+    : "Oops, something went wrong! Please try again.";
+  const imageAlt = props.isSuccess ? "Success" : "Fail";
+
   return (
     <div
       className={`modal modal_tooltip ${props.isOpen ? "modal_active" : ""}`}
     >
       <div className="modal__container">
-        <img className="modal__tooltip-image" src={SuccessMark} alt="Success" />
-        <h3 className="modal__title modal__tooltip-text">
-          Success! You have now been registered.
-        </h3>
+        <img className="modal__tooltip-image" src={image} alt={imageAlt} />
+        <h3 className="modal__title modal__tooltip-text">{text}</h3>
         <button
           className="modal__close-button modal__close_tooltip"
           onClick={props.onClose}
